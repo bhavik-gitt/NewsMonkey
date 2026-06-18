d888b378de3ff4c3676a0c00b2d7dbda05f04b1a
bhavik-gitt <bhavikgithub@gmail.com>
2026-06-18 12:39:58 +0530

    chore: update NewsItem.js

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -9,12 +9,12 @@ const NewsItem  = (props) => {
           <img src={Url} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title ">{title} </h5>
-            <p className="card-text text-c f,.rz">
+            <p className="card-text text-center">
               {description}
             </p>
             <p className="card-text"><small className="text-body-secondary">by {author} on {newDate}</small></p>
             <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark">
-              Read More
+              Read More...
             </a>
           </div>
         </div>
